e648455241a368d032ab89ea8582badd0069a98f
nickguerra <nickg@maliciousgames.com>
2021-09-30 05:29:48 -0500

    Converted note form to use tag helpers

--- Pages/Contact.cshtml.cs
+++ Pages/Contact.cshtml.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WiredBrainCoffee.Models;
 
 namespace WiredBrainCoffee.Pages
 {
     public class ContactModel : PageModel
     {
-        public string Message { get; set; }
+        public Contact Contact { get; set; }
 
         public void OnGet()
         {
-            Message = "Your contact page.";
+      
         }
     }
 }
