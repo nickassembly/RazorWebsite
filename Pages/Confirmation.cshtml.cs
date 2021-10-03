8feceec85bdc77f95567a0510068a8a50e68869a
nickguerra <nickg@maliciousgames.com>
2021-10-03 05:53:19 -0500

    Added slug to replace id in menu items

---
+++ Pages/Confirmation.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiredBrainCoffee.Pages
+{
+    public class ConfirmationModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGetContact()
+        {
+            Message = "Your email was sent to our team.";
+        }
+
+        public void OnGetSubscribe()
+        {
+            Message = "You have been added to the mailing list";
+        }
+    }
+}
--- Pages/Contact.cshtml.cs
+++ Pages/Contact.cshtml.cs
@@ -20,19 +20,21 @@ namespace WiredBrainCoffee.Pages
 
         }
 
-        public void OnPost()
+        public IActionResult OnPost()
         {
             if (ModelState.IsValid)
             {
                 EmailService.SendEmail(Contact);
-                Message = "Your email has been sent.";
+                return new RedirectToPageResult("Confirmation", "Contact");
             }
+
+            return Page();
         }
 
-        public void OnPostSubscribe(string address)
+        public IActionResult OnPostSubscribe(string address)
         {
             EmailService.SendEmail(address);
-            Message = "You have been added to the mailing list.";
+            return new RedirectToPageResult("Confirmation", "Subscribe");
         }
 
     }
